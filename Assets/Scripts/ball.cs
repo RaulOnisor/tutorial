942b7f9585afb2dc741881abd21b68f7456bd5e4
RaulOnisor <raul@firebyte.ro>
2020-02-18 15:28:55 +0200

    initial commit

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ball : MonoBehaviour
+{
+    
+    public float speed = 5f;
+
+    public GameManager manager;
+    public int basicScore = 1;
+    public int specialScore = 5;
+    Vector3 screenHeight;
+    float currentScreenHeight;
+    float scoreMultiplierRatio = 4;
+    float maxScoreMultiplaier;
+   
+    void Start()
+    {
+        screenHeight = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        currentScreenHeight = screenHeight.y;
+        maxScoreMultiplaier = currentScreenHeight / scoreMultiplierRatio;
+        manager = (GameManager)FindObjectOfType(typeof(GameManager));
+    }
+
+    void Update()
+    {
+        transform.Translate(-Vector3.up * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="catcher")
+        {
+
+            if(transform.position.y>maxScoreMultiplaier)
+            {
+                manager.IncreaseScore(basicScore);
+            }
+            else
+            {
+                manager.IncreaseScore(specialScore);
+            }
+            Destroy(this.gameObject);
+        }
+        if(collision.gameObject.tag=="killZone")
+        {
+            Scene cs = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(cs.name);
+        }
+    }
+}
---
+++ Assets/Scripts/speedBall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedBall : MonoBehaviour
+{
+    public float forceAmount;
+    private Rigidbody2D rb;
+    public float ballSpeed = 400f;
+    public bool ballMoving;
+    public Transform ballPos;
+    public float maxBallSpeed = 1000f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+
+        if (ballMoving == false)
+        {
+            transform.position = ballPos.position;
+        }
+
+        if (Input.GetButtonDown("Jump") & ballMoving == false)
+        {
+            ballMoving = true;
+            rb.AddForce(Vector2.up * ballSpeed);
+        }
+        
+        if (ballMoving == true)
+        {
+            ballSpeed += 20f;
+            if (ballSpeed >= maxBallSpeed)
+            {
+                ballSpeed = maxBallSpeed;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Paddle" && ballMoving == true)
+        {
+            rb.AddForce(rb.velocity * forceAmount, ForceMode2D.Force);
+        }
+    }
+}
